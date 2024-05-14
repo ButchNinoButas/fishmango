a699805eb426ae1671369c958d993b265cb01c29
Your Name <butchbutas69@gmail.com>
2024-05-14 22:02:46 +0800

    asadsads

--- app/src/main/java/com/example/fishmango/ButasDashboard.kt
+++ app/src/main/java/com/example/fishmango/ButasDashboard.kt
@@ -2,11 +2,51 @@ package com.example.fishmango
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.fishmango.databinding.ActivityButasDashboardBinding
 
 class ButasDashboard : AppCompatActivity() {
+
+    private lateinit var binding: ButasDashboardBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_butas_dashboard)
+        binding = ButasDashboardBinding.inflate(layoutInflater)
+        setContentView (binding.root)
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId){
+                R.id.Home -> {
+
+                    val intent = Intent(this, ButasDashboard::class.java)
+                    HomeLauncher.launch(intent)
+                    true
+                }
+
+                R.id.Sites -> {
+                    // Handle navigation to My Recipes
+                    val intent = Intent(this, ButasInsightFeature::class.java)
+                    startActivity(intent)
+                    true
+                }
+
+                R.id.User -> {
+                    // Handle navigation to Shopping List
+                    val intent = Intent(this, ButasMyProfileFeature::class.java)
+                    UserLauncher.launch(intent)
+                    true
+                }
+
+                R.id.Store -> {
+                    // Handle navigation to Shopping List
+                    val intent = Intent(this, ButasMarket::class.java)
+                    SettingsLauncher.launch(intent)
+                    true
+                }
+
+                else -> false
+            }
+        }
 
     }
 }
+
+
