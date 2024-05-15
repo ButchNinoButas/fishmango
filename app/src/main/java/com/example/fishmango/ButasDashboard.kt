68801c4885f96c999a5ca90aa1ec5cc2fbe0a87b
ButchNinoButas <butchbutas69@gmail.com>
2024-05-15 19:31:32 +0800

    sadsad

--- app/src/main/java/com/example/fishmango/ButasDashboard.kt
+++ app/src/main/java/com/example/fishmango/ButasDashboard.kt
@@ -2,6 +2,8 @@ package com.example.fishmango
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.fishmango.databinding.ActivityButasDashboardBinding
 
@@ -45,6 +47,43 @@ class ButasDashboard : AppCompatActivity() {
 
                 else -> false
             }
+        }
+
+        val bangus4Button = findViewById<Button>(R.id.bangus4)
+
+
+        bangus4Button.setOnClickListener {
+
+            val intent = Intent(this@ButasDashboard, ButasMarket::class.java)
+            startActivity(intent)
+
+
+        }
+        val til3Button = findViewById<Button>(R.id.til3)
+
+        // Set OnClickListener to the HvAcct TextView
+        til3Button.setOnClickListener {
+
+            val intent = Intent(this@ButasDashboard, ButasMarket::class.java)
+            startActivity(intent)
+        }
+        val till3Button = findViewById<Button>(R.id.till3)
+
+        // Set OnClickListener to SignUpTextView
+        till3Button.setOnClickListener {
+            // Create Intent to start RegisterActivity
+            val intent = Intent(this@ButasDashboard, ButasMarket::class.java)
+            startActivity(intent)
+
+
+        }
+        val tilq3Button = findViewById<Button>(R.id.tilq3)
+
+
+        tilq3Button.setOnClickListener {
+            // Create an Intent to start the LoginActivity
+            val intent = Intent(this@ButasDashboard, ButasMarket::class.java)
+            startActivity(intent)
         }
 
     }
